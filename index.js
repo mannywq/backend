5c94f4b312b2b215edacbda3230c9e2b1de03791
Manny W <manny.wetterqvist@outlook.com>
2022-03-30 14:17:47 +0900

    3.7-3.8

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
-
 app.use(express.json())
+
+morgan.token('custom', (req, res) => {
+
+  return JSON.stringify(req.body)
+
+})
+app.use(morgan(':method :url :status :custom'))
+
 
 let persons = [
         {
