eada55f6faefb8c7f84d4a7919180fe8a170c233
Manny W <manny.wetterqvist@outlook.com>
2022-03-30 22:10:47 +0900

    first server up

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('custom', (req, res) => {
 
@@ -15,22 +18,22 @@ app.use(morgan(':method :url :status :custom'))
 let persons = [
         {
           "name": "Arto Hellas",
-          "number": "040-123456",
+          "phone": "040-123456",
           "id": 1
         },
         {
           "name": "Ada Lovelace",
-          "number": "39-44-5323523",
+          "phone": "39-44-5323523",
           "id": 2
         },
         {
           "name": "Dan Abramov",
-          "number": "12-43-234345",
+          "phone": "12-43-234345",
           "id": 3
         },
         {
           "name": "Mary Poppendieck",
-          "number": "39-23-6423122",
+          "phone": "39-23-6423122",
           "id": 4
         },
         {
